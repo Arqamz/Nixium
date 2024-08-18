fde00053e6b946e3167c155f6abefee92f5533cf
Arqam Zia <arqam.mzia@gmail.com>
2024-08-18 21:31:06 +0500

    Change DNS to 1.1.1.1

--- nixos/imports/networking.nix
+++ nixos/imports/networking.nix
@@ -1,7 +1,15 @@
+
 { config, pkgs, ... }: 
 {
   networking.hostName = "asylum"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+
+  networking.resolvconf.enable = pkgs.lib.mkForce false;
+  networking.dhcpcd.extraConfig = "nohook resolv.conf";
+  networking.networkmanager.dns = "none";
+  services.resolved.enable = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
